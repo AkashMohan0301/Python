c28944b77cdaa647f158fa21d04defb24095da68
Akash Mohan <akashmohan940@gmail.com>
2024-05-15 19:16:52 +0530

    2

---
+++ celcius_to_fahrenheit.py
@@ -0,0 +1,5 @@
+def cel_to_fah(c):
+    f=(c*9/5)+32
+    return f
+c=float(input("Enter the Celcius :"))
+print(cel_to_fah(c))
--- sum_of _two_numbers.py
+++ sum_of _two_numbers.py
@@ -1,5 +1,14 @@
-num1 = float(input("Enter First Number :"))
-num2 = int(input("Enter Second Number :"))
-num3 = num1 + float(num2)
-print(num3)
+# num1 = float(input("Enter First Number :"))
+# num2 = float(input("Enter Second Number :"))
+# num3 = num1 + num2
+# print(num3)   without function
+
+num1 = float(input("Enter First Number :")) #with funtion 
+num2 = float(input("Enter Second Number :"))
+
+def add(num1,num2):
+    c=num1+num2
+    return c
+
+print("sum=",add(num1,num2))
 
